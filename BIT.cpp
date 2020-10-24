11dd92f9eaa0447dd7a50c77a7fdcce8eced596e
morpheus521 <44001225+morpheus521@users.noreply.github.com>
2020-10-24 12:10:15 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -24,15 +24,7 @@ int query(int x){
         //s+=BIT[x];
      return s;
 }
-
-void solve(){
-	
-}
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);	
-	int T=1;
-	//cin>>T;
-	while(T--)
-		solve();
 }
